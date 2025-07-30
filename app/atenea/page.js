c9e9aecf77aa7087939f40bec28f9f22c2d85859
LAB-Cyberware <lab.cyberware@gmail.com>
2025-07-30 15:43:59 -0400

    premium boton obtencion 2

--- app/atenea/page.js
+++ app/atenea/page.js
@@ -248,7 +248,7 @@ Respuesta: [Tu respuesta aquí]`;
           </button>
           <h2 className='space-y-5 mb-5'>Aquí va el componente de pago</h2>
           <span className='flex gap-8'>
-            {user.rol !== 'premium' && (
+            {session.user.rol !== 'premium' && (
               <button  
               onClick={() => cambiarRol(user._id, 'premium')}
               disabled={updatingUser === user._id} 
