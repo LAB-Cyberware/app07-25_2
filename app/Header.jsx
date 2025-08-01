a7120150c5c5990fc30be6bababe0dc8cc2f7771
LAB-Cyberware <lab.cyberware@gmail.com>
2025-08-01 15:56:09 -0400

    boton sesion cerrar funcional

---
+++ app/Header.jsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useSession, signOut } from 'next-auth/react';
+import { LogOut } from 'lucide-react'
+
+export default function Header() {
+    const { data: session } = useSession();
+    if (!session) return null;
+
+    return (
+        <header>
+            <nav className='main-nav'>
+            <button 
+                onClick={() => signOut()}
+                className="group fixed top-4 right-4 z-40 bg-gradient-to-r from-red-500 to-red-600 hover:from-red-600 hover:to-red-700 text-white font-medium py-2 px-4 rounded-lg transition-all duration-300 transform hover:-translate-y-1 hover:shadow-lg active:translate-y-0 focus:outline-none focus:ring-4 focus:ring-red-300/50 overflow-hidden"
+            >
+                <span className="absolute inset-0 bg-gradient-to-r from-transparent via-white/20 to-transparent transform -translate-x-full group-hover:translate-x-full transition-transform duration-700"></span>
+                
+                <span className="relative flex items-center justify-center gap-2">
+                <LogOut className="w-4 h-4" />
+                <span className="text-sm">Cerrar sesión</span>
+                </span>
+            </button>
+            </nav>
+        </header>
+    )
+}
